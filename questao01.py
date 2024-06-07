b6892ee3ea242d30fd04a50322e7908ac6d3fc60
lmc <cavalcal@oregonstate.edu>
2024-06-07 08:51:34 -0300

    terminei a questao 01 com exemplos

---
+++ questao01.py
@@ -0,0 +1,10 @@
+def maior(*numeros):
+    maior_elemento = numeros[0]
+    for i in range(1, len(numeros)):
+        if numeros[i] > maior_elemento:
+            maior_elemento = numeros[i]  
+    print(maior_elemento)
+
+maior(1,2,3,4,5)
+maior(4,7)
+maior(100,1)
